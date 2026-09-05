a1862ebc07e39222e2e548167609a24e99850e72
SejinHer <hersejin@gmail.com>
2026-09-05 11:15:56 +0900

    feature : 플레이어 WASD 움직임 추가

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private InputActions inputActions;
+    private Vector2 moveInput;
+
+    public float moveSpeed;
+
+    void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+
+    void Awake()
+    {
+        inputActions = new InputActions();
+    }
+
+    void Start()
+    {
+        inputActions.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        inputActions.Player.Move.canceled += _ => moveInput = Vector2.zero;
+    }
+
+    void Update()
+    {
+        Vector3 dir = new Vector3(moveInput.x, 0f, moveInput.y);
+        transform.Translate(dir * moveSpeed * Time.deltaTime);
+    }
+}
