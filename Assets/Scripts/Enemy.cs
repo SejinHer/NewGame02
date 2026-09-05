26326ad27d55f944fd42a965713a140b29b27751
SejinHer <hersejin@gmail.com>
2026-09-05 17:06:01 +0900

    feature : Enemy 가 플레이어와 간격을 두며 쫓아오도록 수정

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,7 @@ public class Enemy : MonoBehaviour
 {
     public GameObject target;
     public float moveSpeed;
+    public float chaseDistance = 2f;
 
     void Awake()
     {
@@ -13,7 +14,9 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         Vector3 dir = target.transform.position - transform.position;
-
-        transform.Translate(dir.normalized * moveSpeed * Time.deltaTime);
+        if(dir.magnitude > chaseDistance)
+        {
+            transform.Translate(dir.normalized * moveSpeed * Time.deltaTime);
+        }
     }
 }
